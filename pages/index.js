d69a09fe0e19ba9fa0b7b7f8b38fb60ba3ffe321
KimGaeun0806 <greencloud11@hanyang.ac.kr>
2022-06-27 18:33:47 +0900

    Chapter 2.2 - add css

--- pages/index.js
+++ pages/index.js
@@ -32,16 +32,38 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
+    <div className="container">
       <Seo title="Home" />
       {!movies && <h4>Loading...</h4>}
       {/* movies가 존재하지 않을 때 Loading... 띄움 */}
       {movies?.map((movie) => (
-        <div key={movie.id}>
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
       {/* ? 를 넣어서 movies가 존재하지 않으면 map이 실행되지 않도록 함 */}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
